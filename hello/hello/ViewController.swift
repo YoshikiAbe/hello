e34c605031f2157406a9a88ae4a3efd8a13ca1ce
yoshiki abe <yoshiki.abe@jrits.ricoh.co.jp>
2016-10-30 22:54:32 +0900

    hello!

---
+++ hello/hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by 阿部芳樹 on 2016/10/30.
+//  Copyright © 2016年 yoshiki abe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
